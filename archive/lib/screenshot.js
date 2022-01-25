63f024869e3ccee7f50bf8b12ee8afeee628f4df
gatswagg <95851916+gatswagg@users.noreply.github.com>
2022-01-26 05:22:04 +0800

    Delete archive directory

--- archive/lib/screenshot.js
+++
@@ -1,8 +0,0 @@
-const webShot = require("node-webshot");
-const path = require("path");
-
-const screenshot = (website, cb) => webShot(website, path.join(__dirname, '..', 'public', 'bg.jpeg'), cb);
-
-module.exports = {
-    screenshot,
-}
